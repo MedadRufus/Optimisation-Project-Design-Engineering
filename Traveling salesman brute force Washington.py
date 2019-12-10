41d9c404c33139e346a9badcbcd57c7f0abf0ca6
MedadRufus <medadrufus@gmail.com>
2019-12-10 15:08:54 +0000

    put in a brute force algo for tsp

--- Traveling salesman brute force Washington.py
+++ Traveling salesman brute force Washington.py
@@ -27,7 +27,31 @@ num_points = 10
 
 
 
-points_coordinate = np.random.rand(num_points, 2)  # generate coordinate of points
+
+
+#points_coordinate = np.random.rand(num_points, 2)  # generate coordinate of points
+#print(points_coordinate)
+
+
+points_coordinate = [[0.181,14.9],
+                     [9.06,9.40],
+                     [9.38,29.6],
+                     [10.0,9.77],
+                     [14.0,0.915],
+                     [14.5,10.1],
+                     [14.9,11.8],
+                     [16.5,10.9],
+                     [19.0,22.4],
+                     [19.1,15.6],
+                     [20.0,6.26],
+                     [21.6,10.8],
+                     [24.1,17.3],
+                     [24.5,18.1],
+                     [26.3,9.85]]
+
+
+
+
 matrix = spatial.distance.cdist(points_coordinate, points_coordinate, metric='euclidean')
 
 
